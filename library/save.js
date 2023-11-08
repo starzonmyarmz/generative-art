066f63b859c1c1effc45f619ab0806cc7ae4c438
Daniel Marino <iamdanielmarino@gmail.com>
2023-11-08 10:34:56 -0500

    Add micro library to save artwork

---
+++ library/save.js
@@ -0,0 +1,8 @@
+// Functionality to save canvas locally as PNG
+// Requires P5.js library
+
+document.addEventListener('keydown', ({key}) => {
+  if (key === 's') {
+    saveCanvas(`${document.querySelector('title').innerText}_${Date.now()}`, 'png')
+  }
+})
