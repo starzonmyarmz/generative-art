8809f97f16d0e8902ceb26895dd07089484f92f5
Daniel Marino <iamdanielmarino@gmail.com>
2023-11-08 08:26:59 -0500

    Simplify code

--- 003/script.js
+++ 003/script.js
@@ -1,5 +1,10 @@
+let colors
+let rotations
+
 function setup() {
   createCanvas(400, 400)
+  colors = ["#e63946", "#f1faee", "#a8dadc", "#457b9d", "#1d3557"]
+  rotations = [0, HALF_PI, PI, HALF_PI + PI]
 }
 
 function draw() {
@@ -7,23 +12,10 @@ function draw() {
   noStroke()
 
   tile({ cols: 24 }, ({x, y, tile_width, tile_height}) => {
-    fill(random(["#e63946", "#f1faee", "#a8dadc", "#457b9d", "#1d3557"]))
-
+    fill(random(colors))
     push()
     translate(x + tile_width / 2, y + tile_height / 2)
-    
-    switch (int(random(4))) {
-      case 1:
-        rotate(HALF_PI)
-        break
-      case 2:
-        rotate(PI)
-        break
-      case 3:
-        rotate(HALF_PI + PI)
-        break
-    }
-    
+    rotate(random(rotations))    
     arc(-tile_width / 2, -tile_height / 2, tile_width * 2, tile_height * 2, 0, HALF_PI, CHORD)
     pop()
   })
